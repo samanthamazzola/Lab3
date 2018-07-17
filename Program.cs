ff50e3ca977d8859cf8b7b57acba2d3050f91fc0
samantha <33375727+samanthamazzola@users.noreply.github.com>
2018-07-17 16:02:33 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Lab3
 {
@@ -14,7 +14,7 @@ namespace Lab3
             //input
             if (i % 2 == 0 && i >= 2 && i <=25)
             {
-                Console.WriteLine("Your number is Even!");
+                Console.WriteLine("Your number is Even and less than 25!");
                 Console.ReadLine();
             }
             else if (i % 2 == 0 && i >= 26 && i <= 60)
@@ -24,21 +24,23 @@ namespace Lab3
             }
             else if (i % 2 == 0 && i > 60)
             {
-                Console.WriteLine("Your number is Even!");
+                Console.WriteLine($"{i}; Your number is Even!");
                 Console.ReadLine();
             }
+            /* not needed
             else if (i % 2 == 1 && i > 60)
             {
-                Console.WriteLine("Your number is Odd!");
+                Console.Write("Your number is Odd!");
+                Console.ReadLine();
+            }*/
+            else if (i % 2 == 1)
+            {
+                Console.Write($"{i}; Your number is Odd!");
                 Console.ReadLine();
             }
 
             //processing if statements and nested if statements based on 5 conditions
-
-
             //output
-
-
 
         }
     }
