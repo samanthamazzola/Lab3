fabe559d74f0637eec155c596460881cbef8a38f
samantha mazzola <smazzola7@gmail.com>
2018-07-17 15:53:06 -0400

    Lab3 conditionals if else if statments

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        { 
+            double i;
+
+            Console.WriteLine("Please enter a whole integer between 1 and 100: ");
+            i = Double.Parse(Console.ReadLine());
+
+            //input
+            if (i % 2 == 0 && i >= 2 && i <=25)
+            {
+                Console.WriteLine("Your number is Even!");
+                Console.ReadLine();
+            }
+            else if (i % 2 == 0 && i >= 26 && i <= 60)
+            {
+                Console.WriteLine("Your number is Even!");
+                Console.ReadLine();
+            }
+            else if (i % 2 == 0 && i > 60)
+            {
+                Console.WriteLine("Your number is Even!");
+                Console.ReadLine();
+            }
+            else if (i % 2 == 1 && i > 60)
+            {
+                Console.WriteLine("Your number is Odd!");
+                Console.ReadLine();
+            }
+
+            //processing if statements and nested if statements based on 5 conditions
+
+
+            //output
+
+
+
+        }
+    }
+}
